6ca100a9d5cb402d35de7230a72d096464549066
“Faijan <fak66783@gmail.com>
2026-03-13 03:03:43 +0530

    Added LeeTcode Solutions

---
+++ TwoSum.java
@@ -0,0 +1,13 @@
+public class TwoSum {
+    public int[] twoSum(int[] arr, int target){
+        for (int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]+arr[j]==target){
+                    int ans[] = {i,j};
+                    return ans;
+                }
+            }
+        }
+        return null;
+    }
+}
